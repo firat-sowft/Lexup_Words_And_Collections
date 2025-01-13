4f8815ddd57e0790742b327531e3be84327a5b14
𝓜𝓤𝓗𝓐𝓜𝓜𝓔𝓓 𝓖𝓞̈𝓨𝓜𝓔𝓝 <goymenmhmd@gmail.com>
2025-01-13 23:07:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import string
 app = Flask(__name__)
 CORS(app)
 
-app.config['MONGO_URI'] = 'mongodb+srv://goymenmhmd:nZcPoO4NudGLxApx@cluster0.xue2b.mongodb.net/test?'
+app.config['MONGO_URI'] = 'mongodb+srv://xApx@cluster0.xue2b.mongodb.net/test?'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
