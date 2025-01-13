dae9c8af4a7d4ce2608ba5d0823d30b90afe7358
𝓜𝓤𝓗𝓐𝓜𝓜𝓔𝓓 𝓖𝓞̈𝓨𝓜𝓔𝓝 <goymenmhmd@gmail.com>
2025-01-13 23:07:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ app.config['MONGO_URI'] = 'mongodb+srv://xApx@cluster0.xue2b.mongodb.net/test?'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'darknessssolohack@gmail.com'
-app.config['MAIL_PASSWORD'] = 'dbkx iwtq mfxd upoa'
+app.config['MAIL_USERNAME'] = 
+app.config['MAIL_PASSWORD'] = 'd
 
 mail = Mail(app)
 client = MongoClient(app.config['MONGO_URI'])
